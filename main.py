1c0405e5430682218e98cd3465645f2709f548f0
Bridge <hao980224@qq.com>
2022-01-26 11:57:12 +0800

    update

--- main.py
+++ main.py
@@ -4,10 +4,13 @@
 from mouseclick import *
 from chuangkou import *
 
+
+# 简单演示，复杂点的可看menghuan.py
 def click():
     print("点击此电脑")
     cidiannao = r".\images\click.jpg"
     mc = MouseClick()
-    mc.cv2_click(cidiannao)
+    mc.cv2_click(cidiannao, offset_x=5, offset_y=5)  # 如果不设置offset，默认鼠标偏移为5, 如果有需要，可以改成随机偏移量
+
 
 click()
--- mouseclick.py
+++ mouseclick.py
@@ -14,13 +14,17 @@ class MouseClick():
         return win32api.GetSystemMetrics(0), win32api.GetSystemMetrics(1)
 
     def get_window_info(self, wdname):  #  获取程序窗口名
-        #wdname = u'梦幻西游 - 星云引擎'
+        # wdname = u'梦幻西游 - 星云引擎'
         handle = win32gui.FindWindow(0, wdname)  # 获取窗口句柄
+        # print(handle)
         if handle == 0:
             # text.insert('end', '提示：请打开梦幻西游\n')
             # text.see('end')  # 自动显示底部
+            # print(wdname)
+            # print("请将窗口保持在前台")
             return None
         else:
+            # print("窗口句柄为：" + str(handle))
             return win32gui.GetWindowRect(handle)
 
     def move_click(self, x, y, t=0):  # 移动鼠标并点击左键
@@ -33,14 +37,14 @@ class MouseClick():
             time.sleep(t)
         return 0
 
-    def cv2_click(self, imgpath, offset_x=25, offset_y=25):
+    def cv2_click(self, imgpath, offset_x=25, offset_y=25):   # 鼠标默认偏移25xp
         try:
             im2 = py_cv2(imgpath)
             im3x = im2[1] + offset_x
             im3y = im2[0] + offset_y
             self.move_click(im3x, im3y)
         except TypeError:
-            #print("未找到图片对应坐标点！")
+            print("未找到图片对应坐标点！")
             return 0
 
 
